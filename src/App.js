90ab2e3c29ade3a57a5e861d67ebe75d2bf16bd4
Priyanshup891 <priyanshup891@gmail.com>
2022-10-09 19:03:15 +0530

    navbar_setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <>
+    <Navbar/>
+    <Main/>
+    </>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+        <Content>
+        <div>
+            <h1>resumelux</h1>
+            <NavLinks>
+                <p>Templates</p>
+                <p>Examples</p>
+                <p>Services</p>
+                <p>Magazine</p>
+            </NavLinks>
+        </div>
+            <LoginBtn>Login</LoginBtn>
+        </Content>
+    </NavbarContainer>
+  )
+}
+
+const NavbarContainer = styled.nav`
+width:100%;
+height:70px ;
+padding:1rem 6rem ;
+
+`;
+
+const Content = styled.div`
+display:flex ;
+align-items:center ;
+justify-content:space-between ;
+
+& > div{
+    display:flex ;
+    align-items:center ;
+    gap:2rem;
+
+    h1{
+        font-size:1.3rem ;
+        font-weight:700 ;
+        color:#000 ;
+    }
+}
+`;
+
+const NavLinks = styled.div`
+display:flex ;
+align-items:center ;
+gap:2rem;
+
+p{
+    font-size:0.875rem ;
+    font-weight:500 ;
+    color:#999999 ;
+}
+`;
+
+const LoginBtn = styled.button`
+padding:0.35rem 1.5rem ;
+font-size:0.875rem ;
+background-color:#D3D3D3 ;
+border:none ;
+outline:none ;
+font-weight:600 ;
+`;
+
+export default Navbar
